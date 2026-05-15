f3a200d9faf5cabffc558bb51c84510445577c04
Giraldo Rosales <giraldo@nitrogenx.co>
2026-05-15 16:21:51 -0500

    fix: export luson types

--- src/date/index.ts
+++ src/date/index.ts
@@ -1,2 +1,34 @@
-export * from 'luxon';
+export {
+  DateTime,
+  Duration,
+  FixedOffsetZone,
+  IANAZone,
+  Info,
+  Interval,
+  InvalidZone,
+  Settings,
+  SystemZone,
+  VERSION,
+  Zone
+} from 'luxon';
+export type {
+  DateTimeFormatOptions,
+  DateTimeJSOptions,
+  DateTimeMaybeValid,
+  DateTimeOptions,
+  DateTimeUnit,
+  DurationLike,
+  DurationLikeObject,
+  DurationMaybeValid,
+  DurationObjectUnits,
+  DurationOptions,
+  DurationUnit,
+  IntervalMaybeValid,
+  LocaleOptions,
+  NumberingSystem,
+  ToISOTimeOptions,
+  ToRelativeCalendarOptions,
+  ToRelativeOptions,
+  WeekdayNumbers
+} from 'luxon';
 export * from '../dates/index.js';
